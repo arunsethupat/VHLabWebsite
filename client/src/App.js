0a781fb36b29e2830616688d80cf01d03f6f8261
Arun Sethupat <sethupat.arun.4@gmail.com>
2018-05-06 17:09:04 -0500

    Node + React Framework

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Customers from './components/customers';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Visual Heart Laboratory</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
